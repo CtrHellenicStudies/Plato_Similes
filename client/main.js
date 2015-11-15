6312caa8a1ee3754cca72c3cea6f8deca93a4387
Benn Salter <bennsalter@gmail.com>
2015-11-15 15:21:11 -0500

    first version

---
+++ client/main.js
@@ -0,0 +1,16 @@
+Router.configure({
+    layoutTemplate: 'layout'
+});
+
+Router.route('preface', {
+    path: '/'
+});
+
+Router.route('/toc');
+Router.route('/intro');
+Router.route('/compendium');
+Router.route('/chartA');
+Router.route('/chartBF');
+Router.route('/conclusion');
+Router.route('/appendices');
+Router.route('/biblio');
