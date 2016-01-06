5a6bde2b40ebeb5e76bad0053a128f27dc154e1a
Benn Salter <bennsalter@gmail.com>
2016-01-05 21:35:27 -0500

    lots more pages

--- client/templates/navbar.js
+++ client/templates/navbar.js
@@ -4,6 +4,24 @@ if (Meteor.isClient) {
             var currentRoute = Router.current();
             return currentRoute &&
             template === currentRoute.lookupTemplate() ? 'active' : '';
+        },
+        activeIfTemplateInChart: function () {
+            var currentRoute = Router.current();
+            var this_template = currentRoute.lookupTemplate();
+            if(currentRoute && this_template.indexOf("Chart") != -1 && this_template != "ChartA") {
+                return "active";
+            } else {
+                return "";
+            }
+        },
+        activeIfTemplateInAppendix: function () {
+            var currentRoute = Router.current();
+            var this_template = currentRoute.lookupTemplate();
+            if(currentRoute && this_template.indexOf("Appendix") != -1) {
+                return "active";
+            } else {
+                return "";
+            }
         }
     });
 }
