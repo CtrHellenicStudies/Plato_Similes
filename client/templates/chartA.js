c4297b91ec393ed21e499beef903df2ba0233ff2
Benn Salter <bennsalter@gmail.com>
2016-03-08 22:09:54 -0500

    searching & sorting

---
+++ client/templates/chartA.js
@@ -0,0 +1,27 @@
+Template.ChartA.events({
+    "submit #search_form": function(event) {
+        event.preventDefault();
+        var search_term = event.target.search_term.value;
+        var search_field = event.target.search_field.value;
+        var search = {};
+        search[search_field] = new RegExp(search_term);
+        if(!_.isEmpty(search_term)) {
+            Pages.set("filters", search);
+        }
+    },
+    "submit #sort_form": function(event) {
+        event.preventDefault();
+        var sortby = event.target.sortby.value;
+        var direction = event.target.ascdesc.value;
+        var sort = {};
+        sort[sortby] = parseInt(direction);
+        if(!_.isEmpty(sort)) {
+            Pages.set("sort", sort);
+        }
+    },
+    "click #clear_button": function(event, template) {
+        template.find("#search_term").value = "";
+        template.find("#search_field").value = "none";
+        Pages.set("filters", {});
+    }
+});
